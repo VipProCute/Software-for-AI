8678e579c25a208ab3d250da1a8aa68b45ff0ec9
EggClf <97372109+EggClf@users.noreply.github.com>
2024-03-28 00:26:25 +0700

    front end of home page of user
    make with next js and tailwind css with support from daisyUI

---
+++ book-rent-front-end/src/app/page.tsx
@@ -0,0 +1,20 @@
+import Image from "next/image";
+import TopBar from "./components/TopBar";
+import Book from "./components/Book";
+import Newest from "./components/Newest";
+import Trend from "./components/Trend";
+import Filter from "./components/Filter";
+export default function Home() {
+  return (
+    <div className="space-y-4">
+      <TopBar />
+      <div className="flex space-x-4">
+        <Filter />
+        <div className="space-y-8">
+          <Newest />
+          <Trend />
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ book-rent-front-end/tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "main-color": "#E4D2B9",
+        "semi-color": "#E4d2b9",
+      },
+    },
+  },
+  plugins: [require("daisyui")],
+};
